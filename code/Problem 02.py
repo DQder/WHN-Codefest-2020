a120298e064831fdae58f050c18a4099cec1ed3f
DQder <65232864+DQder@users.noreply.github.com>
2020-05-18 18:55:23 +0300

    updated Problem 2

--- code/Problem 02.py
+++ code/Problem 02.py
@@ -1,15 +1,25 @@
 i = 0
-
+print("Press 'e' to exit")
 while i == 0:
     print("Enter two different numbers")
+    # number input
     Num1 = input("First number: ")
+    # exit sequence
+    if Num1 == "e" or Num2 == "e":
+        exit()
+    # number input
     Num2 = input("Second number: ")
+    # exit sequence
+    if Num1 == "e" or Num2 == "e":
+        exit()
 
-    if Num1 == "" or Num2 == "":
+    # check for errors
+    elif Num1 == "" or Num2 == "":
         print("\nError : You have entered one or more numbers with no value\n")
         continue
     elif Num1 == Num2:
         print("\nError : You have entered two numbers of the same value\n")
+    # output
     else:
         if Num1 > Num2:
             print(Num1, ">", Num2, "\n")
