774b324f5a0d36fc7292830029ee270a15ab8611
DQder <65232864+DQder@users.noreply.github.com>
2020-05-18 17:23:36 +0300

    problem 1 completed

---
+++ Problem 01.py
@@ -0,0 +1,10 @@
+Num1 = float(input("Enter your first number: "))
+Num2 = float(input("Enter your second number: "))
+
+
+def subtractnums():
+    Answer = Num1 - Num2
+    return Answer
+
+
+print(subtractnums())
